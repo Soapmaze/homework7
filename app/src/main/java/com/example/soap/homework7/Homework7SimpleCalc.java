f80f3de18adb5cc204bccc4c41526f8f4e3b9dc2
Soap <lee141567@naver.com>
2016-11-18 06:50:14 +0900

    더하기 구현 완료!

--- app/src/main/java/com/example/soap/homework7/Homework7SimpleCalc.java
+++ app/src/main/java/com/example/soap/homework7/Homework7SimpleCalc.java
@@ -2,12 +2,40 @@ package com.example.soap.homework7;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Homework7SimpleCalc extends AppCompatActivity {
+    EditText input_num1;
+    EditText input_num2;
+    Button btn_plus;
+    Button btn_minus;
+    Button btn_multiply;
+    Button btn_divide;
+    TextView txtview_result;
+    int calc_result=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homework7_simple_calc);
+
+        input_num1 = (EditText) findViewById(R.id.input_num1);
+        input_num2 = (EditText) findViewById(R.id.input_num2);
+        btn_plus = (Button) findViewById(R.id.btn_Plus);
+        btn_minus = (Button) findViewById(R.id.btn_Minus);
+        btn_multiply = (Button) findViewById(R.id.btn_Multiply);
+        btn_divide = (Button) findViewById(R.id.btn_Divide);
+        txtview_result = (TextView) findViewById(R.id.txtView_Result);
+
+        btn_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calc_result = Integer.parseInt(input_num1.getText().toString()) + Integer.parseInt(input_num2.getText().toString());
+                txtview_result.setText("Result : " + String.valueOf(calc_result));
+            }
+        });
     }
 }
