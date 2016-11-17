0330398e32f7a8138713477a2ff0964e7c021403
Soap <lee141567@naver.com>
2016-11-18 06:28:40 +0900

    과제7 디자인 구성 완료!

---
+++ app/src/main/java/com/example/soap/homework7/Homework7SimpleCalc.java
@@ -0,0 +1,13 @@
+package com.example.soap.homework7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Homework7SimpleCalc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework7_simple_calc);
+    }
+}
