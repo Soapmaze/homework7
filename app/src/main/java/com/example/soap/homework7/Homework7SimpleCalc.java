1b9b2587df540654b3b4aa89741912d0a176c43f
soapmaze <lee141567@naver.com>
2016-11-18 11:25:08 +0900

    11월 18일 수업자료#1 완료!

--- app/src/main/java/com/example/soap/homework7/Homework7SimpleCalc.java
+++ app/src/main/java/com/example/soap/homework7/Homework7SimpleCalc.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Homework7SimpleCalc extends AppCompatActivity {
     EditText input_num1;
@@ -14,9 +15,10 @@ public class Homework7SimpleCalc extends AppCompatActivity {
     Button btn_minus;
     Button btn_multiply;
     Button btn_divide;
+    Button btn_mod;
     TextView txtview_result;
-    int num1 =0, num2 = 0;
-    int calc_result=0;
+    double num1 =0, num2 = 0;
+    double calc_result=0;
     View.OnClickListener ClickListen;
 
     @Override
@@ -30,16 +32,22 @@ public class Homework7SimpleCalc extends AppCompatActivity {
         btn_minus = (Button) findViewById(R.id.btn_Minus);
         btn_multiply = (Button) findViewById(R.id.btn_Multiply);
         btn_divide = (Button) findViewById(R.id.btn_Divide);
+        btn_mod = (Button) findViewById(R.id.btn_Mod);
         txtview_result = (TextView) findViewById(R.id.txtView_Result);
 
         ClickListen = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num1 = Integer.parseInt(input_num1.getText().toString());
-                num2 = Integer.parseInt(input_num2.getText().toString());
-
+                if(input_num1.getText().toString().equals("") || input_num2.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "값을 넣으세요.", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    num1 = Double.parseDouble(input_num1.getText().toString());
+                    num2 = Double.parseDouble(input_num2.getText().toString());
+                }
                 switch (v.getId()) {
                     case R.id.btn_Plus :
+
                         calc_result = num1 + num2;
                         break;
                     case R.id.btn_Minus :
@@ -49,7 +57,14 @@ public class Homework7SimpleCalc extends AppCompatActivity {
                         calc_result = num1 * num2;
                         break;
                     case R.id.btn_Divide :
+                        if(num2 == 0) {
+                            Toast.makeText(getApplicationContext(), "0으로 나누면 안됩니다.", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
                         calc_result = num1 / num2;
+                        break;
+                    case R.id.btn_Mod :
+                        calc_result = num1 % num2;
                         break;
                 }
                 txtview_result.setText("Result : " + String.valueOf(calc_result));
@@ -59,5 +74,6 @@ public class Homework7SimpleCalc extends AppCompatActivity {
         btn_minus.setOnClickListener(ClickListen);
         btn_multiply.setOnClickListener(ClickListen);
         btn_divide.setOnClickListener(ClickListen);
+        btn_mod.setOnClickListener(ClickListen);
     }
 }
